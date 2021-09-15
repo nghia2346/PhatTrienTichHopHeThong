392bbcc7f1070e50793a9d82cbaa54671a1bf4b0
nghia2346 <nghiatroll39@gmail.com>
2021-09-15 21:30:35 +0700

    Create Bai1.java

---
+++ Tuan2/Bai1.java
@@ -0,0 +1,12 @@
+import java.util.Scanner;
+
+public class Bai1 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        String x;
+        System.out.println("Nhap vao chuoi ki tu: ");
+        x=sc.nextLine();
+        System.out.println("Chuoi ki tu in hoa la:");
+        System.out.println(x.toUpperCase());
+    }
+}
