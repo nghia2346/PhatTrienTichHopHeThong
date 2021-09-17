c57cc446fd30e2e401c080d246f7642b4f69927c
nghia2346 <nghiatroll39@gmail.com>
2021-09-17 10:12:34 +0700

    Update Bai5.java

--- Tuan2/Bai5.java
+++ Tuan2/Bai5.java
@@ -4,7 +4,15 @@ import java.util.Scanner;
  * Bai5
  */
 public class Bai5 {
-    public static void sumSoDuong(int[] a, int n) {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("Nhap so phan tu cua day: ");
+        int n = scanner.nextInt();
+        int[] a = new int[n];
+        Tong(a, n);
+  
+    }
+    public static void Tong(int[] a, int n) {
         Scanner scanner = new Scanner(System.in);
         int sum = 0;
         for (int i = 0; i < a.length; i++) {
@@ -17,12 +25,5 @@ public class Bai5 {
        }
        System.out.println("Tong cac so duong trong day la: " + sum);
     }
-    public static void main(String[] args) {
-        Scanner scanner = new Scanner(System.in);
-        System.out.print("Nhap so phan tu cua day: ");
-        int n = scanner.nextInt();
-        int[] a = new int[n];
-        sumSoDuong(a, n);
-  
-    }
+
 }
