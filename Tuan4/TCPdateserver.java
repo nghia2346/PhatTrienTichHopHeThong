068b8ede309f606070035b3c79d18d8378850d17
nghia2346 <nghiatroll39@gmail.com>
2021-10-06 14:53:47 +0700

    Update TCPdateserver.java

--- Tuan4/TCPdateserver.java
+++ Tuan4/TCPdateserver.java
@@ -1,16 +1,25 @@
+import java.io.DataInputStream;
+import java.io.DataOutputStream;
 import java.net.ServerSocket;
+import java.net.Socket;
 
 public class TCPdateserver {
     public static void main(String[] args) {
             try {
                 ServerSocket ss = new ServerSocket(9999);
                 System.out.println("Server connected");
-                
+                Socket sk =ss.accept();
+                DataOutputStream out = new DataOutputStream(sk.getOutputStream());
+                DataInputStream in = new DataInputStream(sk.getInputStream());
                 while (true) {
-                    
+                    String ch = in.readUTF();
+                    if (ch=="exit") {
+                        break;
+                    }
+                     
                 }
             } catch (Exception e) {
-                //TODO: handle exception
+               
             }
 
     }
