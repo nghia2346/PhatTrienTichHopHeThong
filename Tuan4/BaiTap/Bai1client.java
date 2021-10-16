37e5bd55e073234867cb7fa1df3d6e596c2c10b2
nghia2346 <nghiatroll39@gmail.com>
2021-10-16 18:04:16 +0700

    Update Bai1client.java

--- Tuan4/BaiTap/Bai1client.java
+++ Tuan4/BaiTap/Bai1client.java
@@ -12,14 +12,14 @@ public class Bai1client {
         System.out.println("Client connected");
         DataInputStream in = new DataInputStream(sk.getInputStream());
         DataOutputStream out = new DataOutputStream(sk.getOutputStream());
-      
+        Scanner sc = new Scanner(System.in);
         while (true) {
             System.out.println("Nhap so tu 0 den 9");
-            int ch = System.in.read();
-            out.write(ch);
+            String ch=  sc.nextLine();
+            out.flush();
+            out.writeUTF(ch);
             String kq = in.readUTF();
             System.out.println("Chuyen qua ki tu :"+ kq);
-          
         }
        } catch (Exception e) {
            //TODO: handle exception
