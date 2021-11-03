d8bbe0878cb2a460c1513d1b6d473b15eb0c7e05
trinhnhannghia <90304539+nghia2346@users.noreply.github.com>
2021-11-03 12:51:14 +0700

    Update

--- Tuan6/CalcInterface.java
+++
@@ -1,8 +0,0 @@
-import java.rmi.Remote;
-import java.rmi.RemoteException;
-
-public interface CalcInterface extends Remote {
-    public int cong(int a, int b) throws RemoteException;
-
-    public int tru(int a, int b) throws RemoteException;
-}
